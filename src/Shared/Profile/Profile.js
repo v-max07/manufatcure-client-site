304664fff7d1811a2e7a7e180fd310a292e4b58e
v-max07 <vusandebnath09@gmail.com>
2022-05-28 17:30:45 +0600

    edit profile page! Sorry I am sike.

--- src/Shared/Profile/Profile.js
+++ src/Shared/Profile/Profile.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 const Profile = () => {
     return (
         <div>
             <h2>Profile Page!!!</h2>
+            <Row>
+                <Col>
+                    <div></div>
+                </Col>
+            </Row>
         </div>
     );
 };
